0725f90fa985dc7439146c21fbc2ad90eb27b74c
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-06-08 21:25:43 -0300

    Update MainOne.jsx
    MainOne concluído

--- Projeto Front-End (Academia)/academia/src/components/MainOne.jsx
+++ Projeto Front-End (Academia)/academia/src/components/MainOne.jsx
@@ -5,8 +5,13 @@ function MainOne() {
         <div className="total">
             <div className="ball">
                 <div className="imagem">
-                    <img src="../../src/assets/treinamento.png" alt="" width="350px" height="350px"/>
+                    <img src="../../src/assets/treinamento.png" alt="" width="350px" height="350px" />
                 </div>
+            </div>
+            <div className="contentMain">
+                <h2>FITNESS CLUB</h2>
+                <p>Adentre ao Fitness Club e descubra um novo mundo de possibilidades para aprimorar sua saúde e bem-estar. Nossa academia oferece um ambiente acolhedor e estimulante, onde cada sessão de treino se torna uma oportunidade de superação pessoal. Com equipamentos modernos e instrutores dedicados, estamos aqui para guiá-lo em cada passo da sua jornada fitness. Prepare-se para se surpreender com os resultados que alcançará e com a comunidade vibrante que o aguarda. Venha fazer parte do Fitness Club e transforme sua vida hoje mesmo.</p>
+                <button>Venha Ser FITNESS</button>
             </div>
         </div>
     )
