fd66544961f8d8661940a382e9ed650c1f4d2ebf
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-06-12 13:35:45 -0300

    Update App.jsx
    Atualização

--- Projeto Front-End (Academia)/academia/src/App.jsx
+++ Projeto Front-End (Academia)/academia/src/App.jsx
@@ -4,6 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import MainOne from './components/MainOne'
+import MainTwo from './components/MainTwo'
+import MainThree from './components/MainThree'
+import Plans from './components/Plans'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +16,10 @@ function App() {
    <div>
     <Header />
     <MainOne />
+    <MainTwo />
+    <MainThree />
+    <Plans />
+    <Footer />
    </div>
   )
 }
