2f99f1783064dd1730ea7fc24ba4b983ab1fa1e3
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-06-18 19:57:08 -0300

    Update main.jsx
    Troca De Rotas

--- Projeto Front-End (Academia)/academia/src/main.jsx
+++ Projeto Front-End (Academia)/academia/src/main.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 import {
   createBrowserRouter,
   RouterProvider
- } from "react-router-dom";
+} from "react-router-dom";
 
+import Home from './Home.jsx';
+import CardPayment from './components/routes/CardPayment.jsx';
+import PaymentMethodMuscle from './components/routes/PaymentMethodMuscle.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/card',
+    element: <CardPayment />,
+  },
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: "/paymuscle",
+    element: <PaymentMethodMuscle />
+  }
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
