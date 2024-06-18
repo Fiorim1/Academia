eb12f273c6f5e74ea194d20ee423fb97ed0ff8b0
Luizhenriquehf <145229556+Luizhenriquehf@users.noreply.github.com>
2024-06-18 19:39:41 -0300

    Add files via upload
    Rota Login

---
+++ Projeto Front-End (Academia)/academia/src/components/routes/login.jsx
@@ -0,0 +1,22 @@
+import "../styles/login.css"
+
+function Login() {
+    return (
+      <div className="login-container">
+        <h2>Login</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default Login;
