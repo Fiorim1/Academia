b09e19808fefd3d05f82155c979fc1ab525832e7
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-06-20 14:41:37 -0300

    Add files via upload
    Novos Arquivos

---
+++ Projeto Front-End (Academia)/academia/src/components/routes/PageMuscle/PageMuscle.jsx
@@ -0,0 +1,16 @@
+import Header from "../../Header"
+import Perfil from "./components/Perfil"
+import Changes from "./components/Changes"
+
+const PageMuscle = () => {
+    return (
+        <div>
+            <Header />
+            <Perfil />
+            <Changes />
+        </div>
+        
+    )
+}
+
+export default PageMuscle
---
+++ Projeto Front-End (Academia)/academia/src/components/routes/PageMuscle/components/Changes.jsx
@@ -0,0 +1,19 @@
+import "../../../../styles/Changes.css"
+
+function Changes() {
+    return (
+        <div className="buttonChange">
+        <div className="training">
+            <button>💪🏽 Treinos</button>
+        </div>
+        <div className="trainer">
+            <button>🏋🏼 Chamar Treinador</button>
+        </div>
+        <div className="suple">
+            <button>🛒 Compre Suplementos</button>
+        </div>
+    </div>
+    )
+}
+
+export default Changes
