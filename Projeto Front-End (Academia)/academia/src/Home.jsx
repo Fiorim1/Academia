109eba1b49723da7c0bba4f0aa701f577456ed35
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-06-23 17:54:02 -0300

    Update Home.jsx
    Atualização Do Novo Header Para a Landing page

--- Projeto Front-End (Academia)/academia/src/Home.jsx
+++ Projeto Front-End (Academia)/academia/src/Home.jsx
@@ -2,19 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header'
 import MainOne from './components/MainOne'
 import MainTwo from './components/MainTwo'
 import MainThree from './components/MainThree'
 import Plans from './components/Plans'
 import Footer from './components/Footer'
+import HeaderPrincipal from './components/HeaderPrincipal'
 
 function Home() {
-  const [count, setCount] = useState(0)
 
   return (
    <div>
-    <Header />
+    <HeaderPrincipal />
     <MainOne />
     <MainTwo />
     <MainThree />
