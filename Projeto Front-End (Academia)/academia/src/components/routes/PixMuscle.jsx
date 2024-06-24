f01b60d93167f2e92e46462b303b452d9c762518
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-06-23 21:33:01 -0300

    Add files via upload

---
+++ Projeto Front-End (Academia)/academia/src/components/routes/PixMuscle.jsx
@@ -0,0 +1,16 @@
+import Header from "../Header"
+import Footer from "../Footer"
+
+function PixMuscle() {
+    return (
+        <div>
+            <Header />
+            <div>
+                
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+
+export default PixMuscle
