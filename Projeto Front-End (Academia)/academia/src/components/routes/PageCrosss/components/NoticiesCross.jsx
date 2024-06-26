4f546bd52dfda7fd5bb21fc25f8b2b7746427c5e
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-06-26 16:37:39 -0300

    Add files via upload

---
+++ Projeto Front-End (Academia)/academia/src/components/routes/PageCrosss/components/NoticiesCross.jsx
@@ -0,0 +1,43 @@
+import "../../../../styles/NoticiesCross.css"
+import { useState, useEffect } from "react";
+
+function NoticiesCross() {
+
+    const images = [
+        'https://via.placeholder.com/600x300?text=Image+1',
+        'https://via.placeholder.com/600x300?text=Image+2',
+        'https://via.placeholder.com/600x300?text=Image+3',
+    ];
+
+    const [currentImage, setCurrentImage] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImage((prevImage) =>
+                prevImage === images.length - 1 ? 0 : prevImage + 1
+            );
+        }, 3000); // Troca de imagem a cada 3 segundos
+
+        return () => clearInterval(interval);
+    }, []);
+
+
+    return (
+        <div className="noticies">
+            <div className="titleNoticies">
+                <h3>Notícias CrossFit</h3>
+            </div>
+            <div className="carousel">
+                {images.map((imageUrl, index) => (
+                    <img
+                        key={index}
+                        src={imageUrl}
+                        alt={`Slide ${index}`}
+                        className={index === currentImage ? 'active' : 'inactive'}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default NoticiesCross
