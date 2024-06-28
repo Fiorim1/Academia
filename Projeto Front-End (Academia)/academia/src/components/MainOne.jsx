21eefe4d8831557bab83478a47ca0f2bbaf9e05e
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-06-28 15:55:02 -0300

    Update MainOne.jsx

--- Projeto Front-End (Academia)/academia/src/components/MainOne.jsx
+++ Projeto Front-End (Academia)/academia/src/components/MainOne.jsx
@@ -1,6 +1,12 @@
 import "../styles/MainOne.css"
+import { useNavigate } from "react-router-dom"
+
 
 function MainOne() {
+
+    function alerPlan() {
+        alert("Escolha um de nossos planos abaixo!")
+    }
     return (
         <div className="total">
             <div className="ball">
@@ -11,7 +17,7 @@ function MainOne() {
             <div className="contentMain">
                 <h2>FITNESS CLUB</h2>
                 <p>Adentre ao Fitness Club e descubra um novo mundo de possibilidades para aprimorar sua saúde e bem-estar. Nossa academia oferece um ambiente acolhedor e estimulante, onde cada sessão de treino se torna uma oportunidade de superação pessoal. Com equipamentos modernos e instrutores dedicados, estamos aqui para guiá-lo em cada passo da sua jornada fitness. Prepare-se para se surpreender com os resultados que alcançará e com a comunidade vibrante que o aguarda. Venha fazer parte do Fitness Club e transforme sua vida hoje mesmo.</p>
-                <button>Venha Ser FITNESS</button>
+                <button onClick={alerPlan}>Venha Ser FITNESS</button>
             </div>
         </div>
     )
